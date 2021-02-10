8cdec1d1edd7326a57045b4a772e44b416c62fc1
Flor2801 <florenciasarria1@hotmail.com>
2021-02-10 19:23:13 -0300

    Conexioncon Marvel OK

--- script.js
+++ script.js
@@ -1,78 +1,30 @@
-// fetch('https://rickandmortyapi.com/api/character')
 
-// .then((data) => {
-//     return data.json();
-// })
+mostrarComics = (info) => {
+let comic = info.data.results;
+const resultados = document.getElementById("resultados");
+const totalComics = document.getElementById("filtrado");
 
-// .then((personajes) => {
-//     const resultados = document.getElementById('resultados');
+totalComics.innerHTML = `${info.data.total}`;
 
-//     resultados.innerHTML = ""
+resultados.innerHTML = "";
 
-//     personajes.results.map ((info) => {
+comic.map((info) => {
+  resultados.innerHTML += `<article class="card"><div class="imagen"><img src="${info.thumbnail.path}.jpg" alt=""></div>
+<div class="info">
+<div class="nombre"><h2>${info.title}</h2></div></div>
+</article>`;
+});
+}
 
-//     resultados.innerHTML +=
-//     `<article class="card"><div class="imagen"><img src="${info.image}" alt=""></div>
-//       <div class="info">
-//       <div class="nombre"><h2>${info.name}</h2></div>
-//       <div class="estado"><p>${info.status}</p>-<p>${info.species}</p></div>
-//       <div class="ubicacion"><p>Location:</p><p>${info.location.name}</p></div>
-//       <div class="episodio"><p>First seen in:</p><p>${info.episode[0]}</p></div>
-//       </article>`
 
-//     })
+fetch(
+  "https://gateway.marvel.com:443/v1/public/comics?&apikey=cdf503fce8f2c519f899f64cff25fd79"
+)
+  .then((data) => {
+    return data.json();
+  })
 
-// })
+  .then((info) => {
+  mostrarComics(info)
+  });
 
-// const primeraPagina = document.getElementById("first")
-// const siguientePagina = document.getElementById("previous")
-// const proximaPagina = document.getElementById("next")
-// const ultimaPagina = document.getElementById("last")
-
-const buscarInfo = (url) => {
-  fetch(url)
-    .then((data) => {
-      return data.json();
-    })
-
-    .then((personajes) => {
-      const proximo = document.getElementById("proximo");
-
-      proximo.onclick = (e) => {
-        e.preventDefault()
-        buscarInfo(personajes.info.next)
-      };
-
-      const resultados = document.getElementById("resultados");
-
-      resultados.innerHTML = "";
-
-      personajes.results.map((info) => {
-        resultados.innerHTML += `<article class="card"><div class="imagen"><img src="${info.image}" alt=""></div>
-        <div class="info">
-        <div class="nombre"><h2>${info.name}</h2></div>
-        <div class="estado"><p>${info.status}</p>-<p>${info.species}</p></div>
-        <div class="ubicacion"><p>Location:</p><p>${info.location.name}</p></div>
-        <div class="episodio"><p>First seen in:</p><p>${info.episode[0]}</p></div>
-        </article>`;
-      });
-    });
-};
-
-buscarInfo("https://rickandmortyapi.com/api/character?page=1");
-
-// primeraPagina.onclick = () => {
-
-// }
-
-// ultimaPagina.onclick = () => {
-
-// }
-
-// primeraPagina.onclick = () => {
-
-// }
-
-// primeraPagina.onclick = () => {
-
-// }
